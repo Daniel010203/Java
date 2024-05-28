3d1dc8dfd0377baefd98836161fa58a9f4751d2e
Daniel de Oliveira Vieira <danieloliveiravieira3@gmail.com>
2024-05-28 16:26:28 -0300

    28/05/2024

--- Projeto Sistema Smart Tv/src/Usuário.java
+++ Projeto Sistema Smart Tv/src/Usuário.java
@@ -2,6 +2,11 @@ public class Usuário {
     public static void main(String[] args) throws Exception {
     SmartTv smartTv = new SmartTv();
 
+       smartTv.diminuirVolume();
+       smartTv.diminuirVolume();
+       smartTv.diminuirVolume();
+       smartTv.aumentarVolume();
+
         System.out.println("Tv Ligada ?" + smartTv.ligada);
         System.out.println("Canal Atual :" + smartTv.canal);
         System.out.println("Voluma Atual: " + smartTv.volume);
