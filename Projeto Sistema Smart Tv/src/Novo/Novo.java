4d0e4cac02b5ef658f2d010f8612a11ff0950b2a
Daniel de Oliveira Vieira <danieloliveiravieira3@gmail.com>
2024-06-07 15:43:45 -0300

    07/06/2024 = Praticando a sintaxe Java.

---
+++ Projeto Sistema Smart Tv/src/Novo/Novo.java
@@ -0,0 +1,15 @@
+package Novo;
+
+public class Novo {
+    public static void main(String[] args) {
+
+        int idade = 25;
+        double preço = 10.0;
+        char sexo = 'M';
+        boolean temFilhos = true;
+        System.out.println(idade);
+        System.out.println(preço);
+        System.out.println(sexo);
+        System.out.println(temFilhos);
+    }
+}
---
+++ Projeto Sistema Smart Tv/src/Novo/Novo2.java
@@ -0,0 +1,18 @@
+package Novo;
+
+public class Novo2 {
+    public static void main(String[] args) {
+        int x = 10;
+        double d = x;
+        long l = x;
+        float l = x;
+        short s = 20;
+        x = s;
+
+        double valorDoublle = 9.99;
+        int valorInt = (int) valorDoublle;
+        System.out.println(valorInt);
+    }
+
+    
+}
