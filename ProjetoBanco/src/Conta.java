9613cc984719e43af1f3165b85a20b9f11a5d590
Daniel de Oliveira Vieira <danieloliveiravieira3@gmail.com>
2024-06-11 15:19:23 -0300

    11 06 2024

--- ProjetoBanco/src/Conta.java
+++ ProjetoBanco/src/Conta.java
@@ -1,5 +1,23 @@
 public class Conta {
-  int numero;
-  double saldo;
-public Object cliente;
+  private int numero;
+  private double saldo;
+
+  public Conta(){
+    
+  }
+
+  public Conta (int numero, double saldo) {
+    this.numero = numero;
+    this.saldo = saldo;
+  } ;
+
+  public double recuperarSaldo(){
+    return saldo;
+  }
+  public void depositar(double valor){
+    saldo = saldo + valor;
+  }
+  public void setNumero(int numero){
+    this.numero = numero;
+  }
 }
--- ProjetoBanco/src/TesteConta.java
+++ ProjetoBanco/src/TesteConta.java
@@ -1,31 +1,9 @@
 public class TesteConta {
-    /**
-     * @param args
-     */
+
     public static void main(String[] args) {
-        
-        Conta contaCorrente = new Conta();
-        Conta contaPoupanca = new Conta();
-        Conta contaInvestimento = new Conta();
+        Conta cc = new Conta(1000,50.0);
+        cc.setNumero(1000);
 
-        contaCorrente.numero = 11;
-        contaCorrente.saldo = 20.5;
-        contaCorrente.cliente = "Leandro";
-        contaCorrente.cliente = 28;
-
-        contaPoupanca.numero = 22;
-        contaPoupanca.saldo = 30;
-        contaPoupanca.cliente = "Fulano";
-        contaPoupanca.cliente = 50;
-
-        contaInvestimento.numero = 33;
-        contaInvestimento.saldo = 40;
-        contaInvestimento.cliente = "Leandro";
-        contaInvestimento.cliente = 60;
-
-        System.out.println(contaCorrente.numero);
-        System.out.println(contaCorrente.saldo);
-        System.out.println(contaCorrente.nome);
-        System.out.println(contaCorrente.idade);
+        System.out.println(cc.recuperarSaldo());
     }
 }
