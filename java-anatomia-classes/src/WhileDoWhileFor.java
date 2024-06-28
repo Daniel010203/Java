8dd4d1ddcb354a14aa13d00f1ba78a9ff8f77b42
Daniel010203 <danieloliveiravieira3@gmail.com>
2024-06-28 12:42:15 -0300

    28 06 2024

---
+++ java-anatomia-classes/src/WhileDoWhileFor.java
@@ -0,0 +1,11 @@
+public class WhileDoWhileFor {
+    public static void main(String[] args) {
+        int numero = 0;
+        while (numero < 10) {
+            numero = numero + 1;
+            System.out.println(numero);
+        }
+    }
+}
+
+
