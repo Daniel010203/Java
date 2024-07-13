ce6f53f386d5d2f737c3ca0cce0145a85ab73061
Daniel010203 <danieloliveiravieira3@gmail.com>
2024-07-13 11:39:41 -0300

    .

--- Formação Java FIAP e DIO/Java/horadosistema/HoraDoSistema/src/IdiomaSistema.java
+++
@@ -1,8 +0,0 @@
-import java.util.Locale;
-
-public class IdiomaSistema {
-    public static void main(String[] args) {
-        Locale locale = Locale.getDefault();
-        System.out.println("Idioma do sistema: " + locale.getDisplayLanguage());
-    }
-}
