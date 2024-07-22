f12dc7fcfefb49e3c87d8fe5aa5a426555d73e41
Daniel de Oliveira Vieira <135991466+Daniel010203@users.noreply.github.com>
2024-07-22 09:08:23 -0300

    Add files via upload

---
+++ PlanoOperadora.java
@@ -0,0 +1,14 @@
+public class PlanoOperadora {
+    public static void main(String[] args) {
+        String plano = "M";
+
+        if (plano == "B"){
+            System.out.println("100 minutos de ligação");
+        }else if (plano =="M"){
+            System.out.println("100 minutos de ligação e Whats e Insta Grátis !");
+        }else if (plano == "T"){
+            System.out.println("100 minutos de ligação,Whats e Insta grátis + 5Gb de YouTube por conta da casa ! ");
+        }
+
+        }
+    }
